be8a0869b273fc3e36659dc6633af3fb2ec7c4a5
Camila <camila99pessoa@gmail.com>
2022-05-17 23:28:07 -0300

    Implemented form data capture

--- js/form.js
+++ js/form.js
@@ -1,27 +1,56 @@
-
-//class contato
-
 class contato {
-    
+
+    constructor(nome, email, telefone, prefContato, tipoContato, mensagem) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.prefContato = prefContato;  
+        this.tipoContato = tipoContato; 
+        this.mensagem = mensagem; 
+    }
 }
 
 function Post(form) {
 
-  let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
+    event.preventDefault(); 
+
+    let data = new contato(form.elements.namedItem("nome").value,
             form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
             form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
-  
+            form.elements.namedItem("prefContato").value,
+            form.elements.namedItem("tipoContato").value,
+            form.elements.namedItem("mensagem").value
+        )
+
+     Validation()
 }
 
-function Enviar() {
+function Validation() {
+    if (prefContato.value != "COMO DESEJA SER CONTATADO*" && tipoContato.value != "TIPO DE CONTATO*") {
+        Enviar();
+        form.reset();
+    } else {
+        let mensagemErro = document.getElementById("mensagemSucessoOuErro"); 
+        mensagemErro.textContent = "Você deixou de prencheer um ou mais dos campos obrigatórios. Por favor, verifique e tente novamente."; 
 
-    var nome = document.getElementById("nomeid");
+        mensagemErro.classList.add("mensagem-erro");
 
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-    }
+        setTimeout(function(){
+            mensagemErro.textContent = "";
+            mensagemErro.classList.remove("mensagem-erro");
+        }, 5000); 
+        }
+    return;
+}
 
+function Enviar(data) {
+    let mensagemSucesso = document.getElementById("mensagemSucessoOuErro"); 
+    mensagemSucesso.textContent = "Obrigado, sr(a) " + nome.value + ', os seus dados foram encaminhados com sucesso!'; 
+
+    mensagemSucesso.classList.add("mensagem-sucesso");
+
+    setTimeout(function(){
+        mensagemSucesso.textContent = "";
+        mensagemSucesso.classList.remove("mensagem-sucesso");
+    }, 5000); 
 }
