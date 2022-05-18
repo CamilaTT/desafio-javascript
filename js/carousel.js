b70cd1cf8d5fb47aa8bf1afcece73fbad89aa565
Camila <camila99pessoa@gmail.com>
2022-05-17 23:59:11 -0300

    Implemented image carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,14 +1,17 @@
-
-
-//carousel
-
 //Array storage class
 let carouselArr = [];
 
-
-//class Carousel
 class Carousel {
 
+    constructor(image, title, url) {
+        this.image = image; 
+        this.title = title; 
+        this.url = url; 
+    }
+    
+    static _sequence;
+    static _size;
+    static _interval; 
     
       
     static Start(arr){
@@ -27,6 +30,16 @@ class Carousel {
     }
 
     static Next(){
-        
+        if(Carousel._sequence < Carousel._size) {
+            let carouselImages = document.getElementById("carousel");
+            let carouselTitleAndUrl = document.getElementById("carousel-title");
+          
+            carouselImages.innerHTML = "<img src ='./img/"+ carouselArr[Carousel._sequence].image +"'></a>";  
+            carouselTitleAndUrl.innerHTML =  "<a href='"+carouselArr[Carousel._sequence].url+"'>" +carouselArr[Carousel._sequence].title+"</a>"//"<a href='"+carouselArr[Carousel._sequence].url +">" + carouselArr[Carousel._sequence].title + "</a>"
+            Carousel._sequence +=1;
+            return;             
+        }
+
+        Carousel._sequence = 0;
     }
 };
